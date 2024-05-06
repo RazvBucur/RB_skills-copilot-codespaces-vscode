1792a0370d88a90ae3901a3f6ae19cf36406135b
RazvBucur <168561702+RazvBucur@users.noreply.github.com>
2024-05-06 16:41:21 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,100 @@
+// Create web server
+const express = require('express')
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+// Install nodemon
+// npm install -g nodemon
+// nodemon app.js
+// http://localhost:3000
+
+// Path: comments.js
+// Create web server
+const express = require('express')
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+// Install nodemon
+// npm install -g nodemon
+// nodemon app.js
+// http://localhost:3000
+
+// Path: comments.js
+// Create web server
+const express = require('express')
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+// Install nodemon
+// npm install -g nodemon
+// nodemon app.js
+// http://localhost:3000
+
+// Path: comments.js
+// Create web server
+const express = require('express')
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+// Install nodemon
+// npm install -g nodemon
+// nodemon app.js
+// http://localhost:3000
+
+// Path: comments.js
+// Create web server
+const express = require('express')
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+// Install nodemon
+// npm install -g nodemon
+// nodemon app.js
+// http://localhost:3000
+
+// Path: comments.js
